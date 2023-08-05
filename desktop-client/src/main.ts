730f9c3f9553c9727f946dbaf0f8db4422f23123
kmtym1998 <kmtym.devloper@gmail.com>
2023-08-05 21:34:50 +0900

    prettiered

--- desktop-client/src/main.ts
+++ desktop-client/src/main.ts
@@ -1,14 +1,14 @@
-import path from "path";
-import { BrowserWindow, app } from "electron";
+import path from 'path';
+import { BrowserWindow, app } from 'electron';
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      preload: path.resolve(__dirname, "preload.js"),
+      preload: path.resolve(__dirname, 'preload.js'),
     },
   });
 
-  mainWindow.loadFile("dist/index.html");
+  mainWindow.loadFile('dist/index.html');
   // mainWindow.webContents.openDevTools({ mode: "detach" });
 };
 
@@ -16,4 +16,4 @@ app.whenReady().then(() => {
   createWindow();
 });
 
-app.once("window-all-closed", () => app.quit());
+app.once('window-all-closed', () => app.quit());
