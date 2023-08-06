db4ddc68360657ce416a903fdaf394f61697ccf8
kmtym1998 <kmtym.devloper@gmail.com>
2023-08-06 19:58:48 +0900

    ハンバーガー消す

--- desktop-client/src/web/components/feature/layout/MenuAppBar.tsx
+++ desktop-client/src/web/components/feature/layout/MenuAppBar.tsx
@@ -1,14 +1,7 @@
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MenuIcon from '@mui/icons-material/Menu';
+import PestControlIcon from '@mui/icons-material/PestControl';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
@@ -19,15 +12,8 @@ type Props = {
   title: string;
 };
 
-const drawerItems = [
-  { name: 'XXXXX', href: '/' },
-  { name: 'YYYYY', href: '/' },
-  { name: 'ZZZZZ', href: '/' },
-];
-
 export const MenuAppBar: React.FC<Props> = ({ title }) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -41,16 +27,6 @@ export const MenuAppBar: React.FC<Props> = ({ title }) => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => setIsDrawerOpen(true)}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {title}
           </Typography>
@@ -63,7 +39,7 @@ export const MenuAppBar: React.FC<Props> = ({ title }) => {
               onClick={handleMenu}
               color="inherit"
             >
-              <AccountCircle />
+              <PestControlIcon />
             </IconButton>
             <Menu
               id="menu-appbar"
@@ -80,8 +56,8 @@ export const MenuAppBar: React.FC<Props> = ({ title }) => {
               open={Boolean(anchorEl)}
               onClose={handleClose}
             >
-              <MenuItem onClick={handleClose}>ログアウト</MenuItem>
               <MenuItem onClick={handleClose}>個人設定</MenuItem>
+              <MenuItem onClick={handleClose}>検索条件</MenuItem>
               <MenuItem
                 onClick={() => {
                   window.location.href = 'spotify:track:3RWjhGoGLeJlrPlMWJyHJf';
@@ -93,27 +69,6 @@ export const MenuAppBar: React.FC<Props> = ({ title }) => {
           </div>
         </Toolbar>
       </AppBar>
-
-      <Drawer
-        anchor="left"
-        open={isDrawerOpen}
-        onClose={() => setIsDrawerOpen(false)}
-      >
-        <Box sx={{ width: 250 }} role="presentation">
-          <List sx={{ m: 0, p: 0 }}>
-            {drawerItems.map((item, i) => (
-              <React.Fragment key={item.name}>
-                <ListItem disablePadding>
-                  <ListItemButton onClick={() => setIsDrawerOpen(false)}>
-                    <ListItemText primary={item.name} />
-                  </ListItemButton>
-                </ListItem>
-                {i !== drawerItems.length - 1 && <Divider />}
-              </React.Fragment>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
     </Box>
   );
 };
